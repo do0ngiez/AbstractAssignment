0f101416cb8aacb4a41a7e7fecc6344355e7bd68
Kriszha Real <yujmin901205@gmail.com>
2024-10-04 03:35:53 +0800

    Add project files.

---
+++ AbstractAssignment/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAssignment
+{
+    //ABSTRACT AND POLYMORPHISM ASSIGNMENT
+    abstract class Person
+    {
+        public string fName { get; set; }
+        public string lName { get; set; }
+
+        //abstract method
+        public abstract void SayName();
+    }
+
+    //employee inheriting from person
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine($"Name: {fName} {lName}");
+            Console.ReadLine();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine($"{fName} {lName} has quit the job.");
+        }
+    }
+
+    interface IQuittable
+    {
+        void Quit();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee()
+            {
+                fName = "Sample",
+                lName = "Student"
+            };
+
+            employee.SayName();
+
+            IQuittable quittable = employee;
+            quittable.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
